4b8905bed6033d877ecc33e021ee7356fb997706
gunny26 <arthur.messner@gmail.com>
2013-12-22 23:06:34 +0000

    improvements

--- BipolarStepperMotor.py
+++ BipolarStepperMotor.py
@@ -2,21 +2,44 @@
 import RPi.GPIO as GPIO
 import time
 import curses
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import traceback
+import math
 
 class BipolarStepperMotor(object):
+    """
+    Class to represent a bipolar stepper motor
+    it could only with on one dimension, forward or backwards
 
-    def __init__(self, coils, delay):
+    cloil -> set(a1, a2, b1, b2) of GPIO Pins where these connectors are patched
+    delay -> int(milliseconds to wait between moves
+    max_position -> int(maximum position) is set to safe value of 1
+    min_position -> int(minimum position) is set to 0
+    """
+
+    def __init__(self, coils, delay, max_position=1, min_position=0):
+        """init"""
+        # original one from adafruit
         self.sequence = [(1,0,1,0), (0,1,1,0), (0,1,0,1), (1,0,0,1)]
+        # trial, but does not work properly
+        self.sequence = [(1,0,1,0), (0,0,1,0), (0,1,1,0), (0,1,0,0), (0,1,0,1), (0,0,0,1), (1,0,0,1), (1,0,0,0)]
+        # ok
+        self.sequence = [(1,0,0,0), (0,0,1,0), (0,1,0,0), (0,0,0,1)]
+        # also ok
+        self.sequence = [(1,0,0,0), (1,0,1,0), (0,0,1,0), (0,1,1,0), (0,1,0,0), (0,1,0,1), (0,0,0,1), (1,0,0,1)]
         self.num_sequence = len(self.sequence)
         self.coils = coils
         for pin in self.coils:
             GPIO.setup(pin, GPIO.OUT)
         self.delay = int(delay) / 1000.0
         self.position = 0
-        self.max_position = None
-        self.min_position = None
+        self.max_position = max_position
+        self.min_position = min_position
 
     def sign(self, count):
+        """return sign of value as either positive or negative 1"""
         if count > 0:
             return(1)
         elif count < 0:
@@ -24,7 +47,15 @@ class BipolarStepperMotor(object):
         else:
             return(0)
 
-    def move(self, direction, steps):
+    def move(self, direction, steps, delay_faktor=1):
+        """
+        move to given direction number of steps, its relative
+        delay_faktor could be set, if this Motor is connected to a controller
+        which moves also another Motor
+        """
+        assert type(direction) == int
+        assert -1 <= direction <= 1
+        assert type(steps) == int
         for _ in range(steps):
             self.position += direction
             phase = self.sequence[self.position % self.num_sequence]
@@ -32,48 +63,93 @@ class BipolarStepperMotor(object):
             for pin in self.coils:
                 GPIO.output(pin, phase[counter])
                 counter += 1
-            time.sleep(self.delay)
+            time.sleep(self.delay / delay_faktor)
 
     def unhold(self):
+        """
+        sets any pin of motor to low, so no power is needed
+        """
         for pin in self.coils:
             GPIO.output(pin, False)
 
     def get_position(self):
+        """
+        return actual position
+        """
         return(self.position)
 
     def set_position(self, position):
+        """
+        set actual position without movin, for calibration use only
+        """
+        assert type(position) == int
         self.position = position
 
     def set_min(self):
+        """set minimum value, for calibration use only"""
         self.min_position = self.position
 
     def set_max(self):
+        """set maximum value, for calibration use only"""
         self.max_position = self.position
 
     def goto(self, target):
-        """got from actual position to target"""
+        """go from actual position to targeti, target is given absolute"""
+        assert type(target) == int
         steps = target - self.position
         direction = self.sign(steps)
         self.move(direction, abs(steps))
 
     def run(self):
-        """runs from max to min and back"""
+        """runs from max to min and back, for testing"""
         if (self.max_position is not None) and (self.min_position is not None):
             self.goto(self.min_position)
             self.goto(self.max_position)
 
+class Point(object):
+    """arbitrary point class"""
+
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+    def get(self):
+        return((self.x, self.y))
+
+    def getx(self):
+        return(self.x)
+
+    def gety(self):
+        return(self.y)
+
+    def getx_int(self):
+        return(int(round(self.x)))
+
+    def gety_int(self):
+        return(int(round(self.y)))
+
+    def set(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return("(%s, %s)" % (self.x, self.y))
+
 class XYController(object):
+    """controls 2 Motors"""
 
     def __init__(self, motorx, motory):
-        """controls two motors, to coordinate movements"""
+        """controls two motors, two coordinate movements"""
         self.motorx = motorx
         self.motory = motory
-        self.x_float = 0.0
-        self.y_float = 0.0
-        # ((0,0), (1024, 768))
-        self.boundaries = [[None, None], [None, None]]
+        # internal coordinates are stored in float
+        self.position = Point()
+        # after calibration set ((0,0), (1024, 768))
+	self.min_boundary = Point()
+	self.max_boundary = Point()
 
     def sign(self, count):
+        """returns sign of count as either +1 or -1"""
         if count > 0:
             return(1)
         elif count < 0:
@@ -81,104 +157,200 @@ class XYController(object):
         else:
             return(0)
 
-    def move(self, x, y):
-        stepx = self.x_float + x - int(self.x_float)
-        stepy = self.y_float + y - int(self.y_float)
-        direction_x = self.sign(stepx)
-        direction_y = self.sign(stepy)
-        self.motorx.move(direction_x, abs(int(stepx)))
-        self.motory.move(direction_y, abs(int(stepy)))
-        self.x_float += x
-        self.y_float += y
+    def move(self, stepx, stepy):
+        """
+        move to given x,y ccordinates
+        x,y are given relative to actual position
+
+        so to move in both direction at the same time,
+        parameter x or y has to be sometime float
+        """
+        assert type(stepx) == float
+        assert type(stepy) == float
+        assert -1.0 <= stepx <= 1.0
+        assert -1.0 <= stepy <= 1.0
+        # if all both axis are moved, the delay could be halfed
+        delay_faktor = 1
+        if stepx <> 0.0 and stepy <> 0.0:
+            delay_faktor = 2
+        if stepx <> 0.0:
+            # get only the rational part of the new position
+            microstep = self.position.x + stepx - self.position.getx_int()
+            direction = self.sign(stepx)
+            self.motorx.move(direction, int(abs(round(microstep))), delay_faktor)
+            self.position.x += stepx
+        if stepy <> 0.0:
+            microstep = self.position.y + stepy - self.position.gety_int()
+            direction = self.sign(stepy)
+            self.motory.move(direction, int(abs(round(microstep))), delay_faktor)
+            self.position.y += stepy
 
     def set_max_x(self):
+        """set maximum x coordinates"""
         self.motorx.set_max()
-        self.boundaries[1][0] = self.x_float
+        self.max_boundary.x = self.position.x
 
     def set_min_x(self):
+        """set minimum x coordinates"""
         self.motorx.set_min()
-        self.boundaries[0][0] = self.x_float
+        self.min_boundary.x = self.position.x
 
     def set_max_y(self):
+        """set maximum y coordinates"""
         self.motory.set_max()
-        self.boundaries[1][1] = self.y_float
+        self.max_boundary.y = self.position.y
 
     def set_min_y(self):
+        """set minimum y coordinates"""
         self.motory.set_min()
-        self.boundaries[0][1] = self.y_float
+        self.min_boundary.y = self.position.y
 
     def goto(self, target_x, target_y):
-        lengthx = target_x - self.x_float
-        lengthy = target_y - self.y_float
-        steps = int(max(lengthx, lengthy))
-        stepx = lengthx / steps
-        stepy = lengthy / steps
-        for i in range(steps):
-            self.move(stepx, stepy)
+        """goto abosulte x,y position"""
+        assert type(target_x) == int
+        assert type(target_y) == int
+        lengthx = target_x - self.position.getx()
+        lengthy = target_y - self.position.gety()
+        # maximum steps = maximum(length(x), length(y))
+        steps = max(abs(lengthx), abs(lengthy))
+        if steps > 0:
+            stepx = float(lengthx / steps)
+            stepy = float(lengthy / steps)
+            for i in range(int(steps)):
+                self.move(stepx, stepy)
 
     def run(self):
-        if self.boundaries != ((None, None), (None, None)):
-            self.goto(self.boundaries[0][0], self.boundaries[0][1])
-            self.goto(self.boundaries[1][0], self.boundaries[1][1])
+        """goto every boundary, for testing purposes"""
+        # left up corner
+        self.goto(self.max_boundary.x, self.max_boundary.y)
+        self.goto(self.max_boundary.x, self.min_boundary.y)
+        self.goto(self.min_boundary.x, self.max_boundary.y)
+        self.goto(self.min_boundary.x, self.min_boundary.y)
+
+    def get_max(self):
+        """get maximum point"""
+        return(self.max_boundary)
+
+    def get_min(self):
+        """get minimum point"""
+        return(self.min_boundary)
+
+    def get_boundaries(self):
+        """get area"""
+        return((self.get_min(), self.get_max()))
+
+    def get_position(self):
+        """get actual position"""
+        return(self.position)
+
+    def unhold(self):
+        """unhold motor to prevent power consumption"""
+        self.motorx.unhold()
+        self.motory.unhold()
 
 def main():
+    # bring GPIO to a clean state
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
 
+    # enable lm293d with pin 18 HIGH
     enable_pin = 18
     GPIO.setup(enable_pin, GPIO.OUT)
     GPIO.output(enable_pin, 1)
 
-    delay = 10
-    motorx = BipolarStepperMotor((4, 17, 23, 24), delay)
-    motory = BipolarStepperMotor((27, 22, 10, 9), delay)
+    # delay in ms (milliseconds) for phase change
+    delay = 1.0
+    # first motor on
+    # a1=4
+    # a2=17
+    # b1=23
+    # b2=24
+    motorx = BipolarStepperMotor((4, 17, 23, 24), delay, 512)
+    # second motor
+    # a1=27
+    # a2=22
+    # b1=10
+    # b2=9
+    motory = BipolarStepperMotor((27, 22, 10, 9), delay, 512)
+    # let controller handle these two motors
     controller = XYController(motorx, motory)
 
-
+    # initialize curses screen
     stdscr = curses.initscr()
     curses.cbreak()
     stdscr.keypad(1)
-
-    stdscr.addstr(0,10,"Hit 'q' to quit")
+    stdscr.addstr(0,10,"Hit 'p' to quit")
     stdscr.refresh()
 
+    # calculate a circle
+    points = []
+    step = math.pi / 360
+    for degree in range(360):
+        x = int(256 + math.cos(degree * step) * 250)
+        y = int(256 + math.sin(degree * step) * 250)
+        points.append((x, y))
+
     key = ''
-    while key != ord('q'):
+    while key != ord('p'):
         stdscr.refresh()
         key = stdscr.getch()
         stdscr.addch(14, 10, key)
-        if key == curses.KEY_LEFT:
+        if key == ord("a"):
             controller.move(-1, 0)
-        elif key == curses.KEY_RIGHT: 
+        elif key == ord("d"): 
             controller.move(1, 0)
-        elif key == curses.KEY_UP: 
+        elif key == ord("w"): 
             controller.move(0, 1)
-        elif key == curses.KEY_DOWN: 
-            controller.move(0, -1)
         elif key == ord("x"): 
-            controller.set_min_x()
-        elif key == ord("X"): 
-            controller.set_max_x()
+            controller.move(0, -1)
+        elif key == ord("q"): 
+            controller.move(-1, 1)
+        elif key == ord("e"): 
+            controller.move(1, 1)
         elif key == ord("y"): 
-            controller.set_min_y()
+            controller.move(-1, -1)
+        elif key == ord("c"): 
+            controller.move(1, -1)
+        elif key == ord("s"): 
+            controller.unhold()
+        elif key == ord("X"): 
+            controller.goto(512, 0)
+            controller.goto(0, 0)
         elif key == ord("Y"): 
-            controller.set_max_y()
-        elif key == ord("r"): 
-            controller.run()
-        elif key == ord("R"): 
-            motory.run()
-        stdscr.addstr(10, 2, "X-Position : %10s" % motorx.get_position())
-        stdscr.addstr(11, 2, "Y-Position : %10s" % motory.get_position())
-        stdscr.addstr(12, 2, "X-Min      : %10s" % motorx.min_position)
-        stdscr.addstr(13, 2, "X-Max      : %10s" % motorx.max_position)
-        stdscr.addstr(14, 2, "Y-Min      : %10s" % motory.min_position)
-        stdscr.addstr(15, 2, "Y-Max      : %10s" % motory.max_position)
-        stdscr.addstr(16, 2, "ControllerX: %10s" % controller.x_float)
-        stdscr.addstr(17, 2, "ControllerY: %10s" % controller.y_float)
-        stdscr.addstr(18, 2, "Controller : %s" % str(controller.boundaries))
+            controller.goto(0, 512)
+            controller.goto(0, 0)
+        elif key == ord("C"): 
+            controller.goto(512, 256)
+            controller.goto(0, 0)
+            controller.goto(256, 512)
+            controller.goto(0, 0)
+        elif key == ord("V"):
+            controller.goto(0, 256)
+            for point in points:
+                controller.goto(point[0], 256)
+            controller.goto(0, 0)
+            for point in points:
+                controller.goto(256, point[1])
+            controller.goto(0, 0)
+            for point in points:
+                controller.goto(point[0], point[1])
+            controller.goto(0, 0)
+        stdscr.addstr(10, 2, "M-Position   : %s, %s" % (motorx.get_position(), motory.get_position()))
+        stdscr.addstr(11, 2, "M-X(min/max) : %s, %s" % (motorx.min_position, motorx.max_position))
+        stdscr.addstr(12, 2, "M-Y(min/max) : %s, %s" % (motory.min_position, motory.max_position))
+        stdscr.addstr(13, 2, "C-Position   : %s" % str(controller.get_position()))
+        stdscr.addstr(14, 2, "C-(min/max)  : %s" % str(controller.get_boundaries()))
+        controller.unhold()
         time.sleep(delay / 1000.0)
     curses.endwin()
     GPIO.cleanup()
 
 if __name__ == "__main__":
-    main()
+    try:
+       main()
+    except Exception, exc:
+       curses.endwin()
+       GPIO.cleanup()
+       traceback.print_exc()
+       raise exc
+       sys.exit(1)
